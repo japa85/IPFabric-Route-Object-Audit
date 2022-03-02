feb91901d4b701a90240b1bc0a1c1656377396db
Jamie <87706545+japa85@users.noreply.github.com>
2022-03-02 13:00:13 +0000

    Add files via upload

---
+++ IPFabric_Connector.py
@@ -0,0 +1,149 @@
+import requests,sys
+import json
+from pprint import pprint
+
+requests.packages.urllib3.disable_warnings()
+
+#define IPFabric Objects
+
+class obj_IPFabric:
+
+    def __init__(self, servername, username, password):
+
+        # perform inital API Authentication
+
+        global serverName, snapshotId
+        
+        serverName = servername + '/api/v1/'
+        snapshotId = '$last'
+        authData = { 'username': username, 'password' : password }
+        
+        authEndpoint = serverName + 'auth/login'
+        authPost = requests.post(authEndpoint, json=authData, verify=False)
+        if not authPost.ok:
+            print('Unable to authenticate: ' + authPost.text)
+            sys.exit()
+
+        # Collecting the accessToken 
+        accessToken = authPost.json()['accessToken']
+
+        # Creating the tokenHeaders
+
+        global tokenHeaders 
+        tokenHeaders = { 'Authorization' : 'Bearer ' + accessToken}
+        
+
+    def api_post (self, EndPoint_Short, Payload):
+
+        # Module to make REST Posts
+        EndPoint = serverName + EndPoint_Short
+        
+        req = requests.post(EndPoint, headers=tokenHeaders, json=Payload, verify=False)
+        if not req.ok:
+            print ("Error:")
+            pprint(json.loads(req.text))
+            sys.exit()
+        return req
+        
+
+    def api_get (self, EndPoint_Short):
+
+        # Module to make REST Posts
+
+        EndPoint = serverName + EndPoint_Short
+        
+        req = requests.get(EndPoint, headers=tokenHeaders, verify=False)
+        if not req.ok:
+            print ("Error:")
+            pprint(json.loads(req.text))
+            sys.exit()
+        return req       
+
+
+    def api_patch (self, EndPoint_Short, Payload):
+
+        # Module to make REST Posts
+
+        EndPoint = serverName + EndPoint_Short
+        
+        req = requests.patch(EndPoint, headers=tokenHeaders, json=Payload, verify=False)
+        if not req.ok:
+            print ("Error:")
+            pprint(json.loads(req.text))
+            sys.exit()
+        return req
+
+
+    def get_configs(self, limit=5000):
+
+        # return a list of devices using pre-defined filters for nxos and ios/ios-xe devices
+        
+        EndPoint = "tables/inventory/devices"
+        Payload = { "columns":
+                        ["hostname"],
+                    "filters": {"and": [{"vendor": ["eq","cisco"]},{"or": [{"family": ["eq","ios"]},{"family": ["eq","ios-xe"]},{"family": ["eq","nx-os"]}]}]},
+                    "pagination":
+                        { "limit": limit,  "start": 0 },
+                    "snapshot": snapshotId,
+                    "reports": "/inventory/devices"
+                    }
+        
+        output = self.api_post(EndPoint, Payload)
+        
+        tmp_dict_devices = output.json()['data']
+
+        # reformat output to make it easier to get associated hash value later
+        tmp_device_hash = dict()
+        for var in tmp_dict_devices:
+            tmp_device_hash[var['hostname']] = str()
+
+        # get config hashs, cant finder on anything other than hostname, so will need to collect and
+        # and sort ourselves
+        EndPoint = "/tables/management/configuration"
+        Payload = {
+            "columns":[
+                "hash",
+                "hostname",
+                ],
+            "snapshot":snapshotId
+            }
+
+        output = self.api_post(EndPoint, Payload)
+        config_hash = json.loads(output.text)['data']
+
+        # iterated through config hash values and if the hostname for the hash is in the
+        # original device list, add the hash to the list
+        for var in config_hash:
+            if var['hostname'] in tmp_device_hash:
+                tmp_device_hash[var['hostname']] = var['hash']
+
+        # as hostnames in IPFabric are returned with the DNS suffix, we want to strip that
+        # to make it easier, we do this into a new dict
+        device_hash = dict()
+
+        for var in tmp_device_hash:
+            hostname = var.split('.')[0].lower()
+            device_hash[hostname] = tmp_device_hash[var]
+
+        # get the config files
+
+        device_configs = dict()
+
+
+        print ('Getting Configs;')
+        for device in device_hash:
+            print (' > {}'.format(device))
+            EndPoint = "/tables/management/configuration/download?hash={}".format(device_hash[device])
+            output = self.api_get(EndPoint)
+            device_configs[device] = {'config':output.text}
+        print ('All Configs Extracted\n\n')
+
+        return device_configs
+    
+
+
+
+
+    
+
+    
---
+++ Routing_Config_Audit.py
@@ -0,0 +1,127 @@
+from IPFabric_Connector import obj_IPFabric
+from pprint import pprint
+import re
+from datetime import datetime
+
+ipf_servername = "https://ipfabric.nw.mgmt.local"
+ipf_username   = "apiaccess"
+ipf_password   = "apiaccess"
+
+Deivce_Limit = 5000
+
+# create regex matches for route-maps, when a route-map is defined,
+# 'route-map' appears at the start of line, whereas  when a route-map is referenced,
+# it always has 1+ space infront of the 'route-map' command
+reg_route_map_defined    = re.compile(r'^route-map (\S+)\s')
+reg_route_map_referenced = re.compile(r'.* route-map (\S+)')
+
+reg_preifx_list_defined    = re.compile(r'ip(?:v6)* prefix-list (\S+)')
+reg_preifx_list_referenced = re.compile(r'.*match ip(?:v6)* address prefix-list (\S+)')
+
+# Use IPFabric to get the device configs
+# note, this function has a defined filter for NX-OS, IOS and IOS-XE devices hardcoded
+def get_device_configs():
+    IP_Fabric = obj_IPFabric(ipf_servername, ipf_username, ipf_password)
+    config_dict = IP_Fabric.get_configs(limit=Deivce_Limit)
+    return config_dict
+
+
+# function to look at the config and extract route-map information
+def extract_info(config_dict):
+    for device in config_dict:
+
+        # load config into temp list (one line per list element)
+        tmp_config = config_dict[device]['config'].split('\n')
+
+        # define route_map lists for use later
+        route_maps_defined    = list()
+        route_maps_referenced = list()
+
+        prefix_lists_defined    = list()
+        prefix_lists_referenced = list()
+        
+        # iterate through each line of the config
+        for line in tmp_config:
+            # Get defined route-maps
+            if re.match(reg_route_map_defined, line):
+                route_maps_defined.append (re.findall(reg_route_map_defined, line)[0])
+            # Get referenced route-maps
+            if re.match(reg_route_map_referenced, line):
+                route_maps_referenced.append (re.findall(reg_route_map_referenced, line)[0])
+
+        for line in tmp_config:
+            if re.match(reg_preifx_list_defined, line):
+                prefix_lists_defined.append (re.findall(reg_preifx_list_defined, line)[0])
+            if re.match(reg_preifx_list_referenced, line):
+                prefix_lists_referenced.append (re.findall(reg_preifx_list_referenced, line)[0])
+
+        # dedup lists
+        route_maps_defined    = list(dict.fromkeys(route_maps_defined))
+        route_maps_referenced = list(dict.fromkeys(route_maps_referenced))
+
+        prefix_lists_defined    = list(dict.fromkeys(prefix_lists_defined))
+        prefix_lists_referenced = list(dict.fromkeys(prefix_lists_referenced))
+
+        # add info to config_dict
+        config_dict[device]['Route_Maps_Defined']    = route_maps_defined
+        config_dict[device]['Route_Maps_Referenced'] = route_maps_referenced
+
+        config_dict[device]['Prefix_Lists_Defined']    = prefix_lists_defined
+        config_dict[device]['Prefix_Lists_Referenced'] = prefix_lists_referenced
+
+    return config_dict
+
+def audit_config(config_dict):
+    print ('\n\nRoute-Maps defined but not referenced:')
+
+    sortednames=sorted(config_dict.keys(), key=lambda x:x.lower())
+    
+    for device in sortednames:
+        # extract route-map info to make it easier to read
+        route_maps_defined    = config_dict[device]['Route_Maps_Defined']
+        route_maps_referenced = config_dict[device]['Route_Maps_Referenced']
+       
+
+        for rm in route_maps_defined:
+            if rm not in route_maps_referenced:
+                print (' > {} - {}'.format(device, rm))
+
+    print ('\n\nRoute-Maps referenced but not defined:')
+    for device in sortednames:
+        # extract route-map info to make it easier to read
+        route_maps_defined    = config_dict[device]['Route_Maps_Defined']
+        route_maps_referenced = config_dict[device]['Route_Maps_Referenced']
+        
+        for rm in route_maps_referenced:
+            if rm not in route_maps_defined:
+                print (' > {} - {}'.format(device, rm))
+
+
+    print ('\n\nPrefix-Lists defined but not referenced:')
+    
+    for device in sortednames:
+        # extract route-map info to make it easier to read
+        prefix_lists_defined    = config_dict[device]['Prefix_Lists_Defined']
+        prefix_lists_referenced = config_dict[device]['Prefix_Lists_Referenced']
+       
+
+        for rm in prefix_lists_defined:
+            if rm not in prefix_lists_referenced:
+                print (' > {} - {}'.format(device, rm))
+
+    print ('\n\nPrefix-Lists referenced but not defined:')
+    for device in sortednames:
+        # extract route-map info to make it easier to read
+        prefix_lists_defined    = config_dict[device]['Prefix_Lists_Defined']
+        prefix_lists_referenced = config_dict[device]['Prefix_Lists_Referenced']
+        
+        for rm in prefix_lists_referenced:
+            if rm not in prefix_lists_defined:
+                print (' > {} - {}'.format(device, rm))
+
+
+      
+if __name__ == "__main__":
+    config_dict = get_device_configs()
+    config_dict = extract_info(config_dict)
+    audit_config(config_dict)
